2595aac6d284f92af0db1ed1ad1f9b30f03bf172
jacopok <jacopok@gmail.com>
2022-12-01 17:14:42 +0100

    arbitrary plots

--- plots.py
+++ plots.py
@@ -1,5 +1,3 @@
-from coordinate_conversion import get_time_and_coordinates
-from maximum_likelihood_scenario import M1, M2, get_dynamics, get_t_hp
 from pathlib import Path
 from functools import partial
 import numpy as np
@@ -8,15 +6,14 @@ from tqdm import tqdm
 import ffmpeg
 from scipy.interpolate import interp1d
 
+from coordinate_conversion import get_time_and_coordinates
+from maximum_likelihood_scenario import constrained_prior, unconstrained_prior, Event
+# from whiten import whiten
+
 VID_FRAMES = 1000
 TIMES_PER_PLOT = 100
 TIME_AFTER_MERGER = 500
-
-def get_limits(time: int):
-    times = [0, .40*VID_FRAMES, .45*VID_FRAMES, VID_FRAMES]
-    lims = [200, 200, 30, 30]
-    return interp1d(times, lims, kind='linear')(time)
-
+CMAP = plt.get_cmap('inferno')
 
 def make_vid(
     plot_frame: callable,
@@ -38,7 +35,7 @@ def make_vid(
 
         plt.gcf().suptitle(
             (
-                f"time=-{info['time'][time+TIMES_PER_PLOT]:.2f}s, "
+                f"time={-info['time'][time+TIMES_PER_PLOT]:.2f}s, "
                 f"energy={info['energy'][time+TIMES_PER_PLOT]:.4f}M"
             ),
             y=.9
@@ -62,41 +59,50 @@ def make_vid(
         img.unlink()
     frames_folder.rmdir()
 
-def plot_frame(time, r_1, r_2, times, hp, times_per_plot=TIMES_PER_PLOT):
+def plot_frame(time, r_1, r_2, times, hp, event, get_limits, times_per_plot=TIMES_PER_PLOT):
     
     figsize_multip = 1.5
     fix, axs = plt.subplots(nrows=1, ncols=2, figsize=(16/figsize_multip, 9/figsize_multip), dpi=120*figsize_multip)
     
+    # tails
     axs[0].scatter(
         r_1[0, time:time+TIMES_PER_PLOT], 
         r_1[1, time:time+TIMES_PER_PLOT], 
         alpha=np.arange(TIMES_PER_PLOT) / TIMES_PER_PLOT,
-        s=1.
+        s=1.,
+        color=CMAP(.3),
     )
     axs[0].scatter(
         r_2[0, time:time+TIMES_PER_PLOT], 
         r_2[1, time:time+TIMES_PER_PLOT], 
         alpha=np.arange(TIMES_PER_PLOT) / TIMES_PER_PLOT,
-        s=1.
+        s=1.,
+        color=CMAP(.7),
     )
-    axs[0].scatter(
-        r_1[0, time+TIMES_PER_PLOT], 
-        r_1[1, time+TIMES_PER_PLOT], 
-        s=M1,
-        c='black'
+    
+    c1 = plt.Circle(
+        xy=(
+            r_1[0, time+TIMES_PER_PLOT], 
+            r_1[1, time+TIMES_PER_PLOT],
+        ),
+        radius=2*event.mass_1/event.total_mass,
+        color='black'
     )
-    axs[0].scatter(
-        r_2[0, time+TIMES_PER_PLOT], 
-        r_2[1, time+TIMES_PER_PLOT], 
-        s=M2,
-        c='black'
+    if np.linalg.norm(r_1[:, time+TIMES_PER_PLOT]-r_2[:, time+TIMES_PER_PLOT])<.2:
+        radius_2 = 2
+    else:
+        radius_2 = 2*event.mass_2/event.total_mass
+    c2 = plt.Circle(
+        xy=(
+            r_2[0, time+TIMES_PER_PLOT], 
+            r_2[1, time+TIMES_PER_PLOT],
+        ),
+        radius=radius_2,
+        color='black'
     )
-    axs[0].scatter(
-        r_2[0, time:time+TIMES_PER_PLOT], 
-        r_2[1, time:time+TIMES_PER_PLOT], 
-        alpha=np.arange(TIMES_PER_PLOT) / TIMES_PER_PLOT,
-        s=1.
-    )
+    axs[0].add_patch(c1)
+    axs[0].add_patch(c2)
+
     axs[0].set_aspect('equal')
     lims = get_limits(time)
     axs[0].set_xlim(-lims, lims)
@@ -105,10 +111,10 @@ def plot_frame(time, r_1, r_2, times, hp, times_per_plot=TIMES_PER_PLOT):
     axs[0].set_ylabel('y [M]')
     axs[0].set_title('Physical space orbits')
     
-    axs[1].plot(times[:time+TIMES_PER_PLOT], hp[:time+TIMES_PER_PLOT])
+    axs[1].plot(times[:time+TIMES_PER_PLOT], hp[:time+TIMES_PER_PLOT], c='black')
     axs[1].set_ylim(-1.5, 1.5)
-    axs[1].set_xlabel('Time to merger [s]')
-    axs[1].set_title('Plus polarized waveform')
+    axs[1].set_xlabel('Detector frame time to merger [s]')
+    axs[1].set_title('Plus polarization of the waveform')
     axs[1].set_aspect('equal')
     x_left, x_right = axs[1].get_xlim()
     y_low, y_high = axs[1].get_ylim()
@@ -116,29 +122,58 @@ def plot_frame(time, r_1, r_2, times, hp, times_per_plot=TIMES_PER_PLOT):
     # plt.tight_layout()
     # axs[1].set_ylabel('h_{+} [natural units]')
 
-if __name__ == '__main__':
-    t, r_1, r_2 = get_time_and_coordinates()
-    dyn = get_dynamics()
-    t_full, hp = get_t_hp()
+def find_zoom_index(t, r_1, r_2):
+    for time, rad1, rad2 in zip(t, r_1.T, r_2.T):
+        if np.linalg.norm(rad1-rad2) < 30:
+            return time
+
+def plot_event(event: Event):
+    event.compute()
+    t, r_1, r_2 = get_time_and_coordinates(event)
+    dyn = event.dyn
+    t_full, hp = event.t, event.hp
     
     times = np.linspace(t[60], t[-1]+TIME_AFTER_MERGER, num=VID_FRAMES+TIMES_PER_PLOT)
     times_index = np.arange(VID_FRAMES)
-    times_to_merger = t[-1] - times + TIME_AFTER_MERGER
+    times_to_merger = t[-1] - times
     
     r_1_interp = interp1d(t[:-35], r_1[:,:-35], fill_value=0, bounds_error=False)(times)
     r_2_interp = interp1d(t[:-35], r_2[:,:-35], fill_value=0, bounds_error=False)(times)
     E_interp = interp1d(t, dyn['E'], fill_value=dyn['E'][-1], bounds_error=False)(times)
     hp_interp = interp1d(t_full, hp, kind='cubic', fill_value='extrapolate')(times)
     
-    M_in_seconds = 4.92549095e-06 * (M1+M2)
+    M_in_seconds = 4.92549095e-06 * event.total_mass * (1+event.redshift)
     
+    def get_limits(time: int):
+        N = VID_FRAMES+TIMES_PER_PLOT
+        times = [
+            0, 
+            .3*N, 
+            find_zoom_index(times_index, r_1_interp, r_2_interp)-TIMES_PER_PLOT, 
+            N
+        ]
+        lims = [250, 250, 30, 30]
+        return interp1d(times, lims, kind='linear')(time)
+
     
     make_vid(
-        partial(plot_frame, r_1=r_1_interp, r_2=r_2_interp, times=-times_to_merger * M_in_seconds, hp=hp_interp), 
-        'capture', 
+        partial(
+            plot_frame, 
+            r_1=r_1_interp, 
+            r_2=r_2_interp, 
+            times=-times_to_merger * M_in_seconds, 
+            hp=hp_interp,
+            event=event,
+            get_limits=get_limits,
+        ), 
+        event.title, 
         times_index,
         info={
             'energy': E_interp,
             'time': times_to_merger * M_in_seconds
         },
     )
+
+if __name__ == '__main__':
+    plot_event(constrained_prior)
+    plot_event(unconstrained_prior)
